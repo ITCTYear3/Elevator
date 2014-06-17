36eb8f4c18c18dc1442e2483d179b4ca23f9cab8
jmorgan1 <jack@ehelion.com>
2014-06-16 23:22:26 -0400

    Added save file dialog for logging to file (incomplete)

--- Python/PyLogger/src/logger.py
+++ Python/PyLogger/src/logger.py
@@ -31,7 +31,8 @@ remote_socket = (remote_host, remote_port)
 class SerialClient(threading.Thread):
     """Busy wait watching serial port for new incomming data and pass data to subscriber"""
     def __init__(self, port='COM1', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE):
-        super(SerialClient, self).__init__()
+        threading.Thread.__init__(self)
+        
         self.ser = serial.Serial()
         self.ser.port     = port
         self.ser.baudrate = baudrate
@@ -84,7 +85,8 @@ class SerialClient(threading.Thread):
 class SocketClient(threading.Thread):
     """Listen on local socket and pass received data to subscriber"""
     def __init__(self):
-        super(SocketClient, self).__init__()
+        threading.Thread.__init__(self)
+        
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(local_socket)
         self.socket.listen(5)
@@ -124,62 +126,52 @@ class SocketClient(threading.Thread):
         return timestamp + " " + string
 
 
-class MainWindow(wx.Frame):
-    def __init__(self, parent, title):
-        super(MainWindow, self).__init__(parent, title=title)
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
-        self.InitUI()
-        #self.Center()
-        self.Show()
+class MainPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
         
         pub.subscribe(self.UpdateDisplay, 'update')
         
-        # start thread to listen for incomming socket connections
-        self.ipc = SocketClient()
+        self.InitUI()
     
     def InitUI(self):
-        """Setup window elements"""
-        panel = wx.Panel(self)
-        
-        vbox = wx.BoxSizer(wx.VERTICAL)
+        self.mainSizer = wx.BoxSizer(wx.VERTICAL)
         
         hbox1 = wx.BoxSizer(wx.HORIZONTAL)
-        textTitle = wx.StaticText(panel, label="Logs")
+        textTitle = wx.StaticText(self, label="Logs")
         hbox1.Add(textTitle)
-        vbox.Add(hbox1, flag=wx.LEFT|wx.TOP)
+        self.mainSizer.Add(hbox1, flag=wx.LEFT|wx.TOP, border=5)
         
         hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        self.textDisplay = wx.TextCtrl(panel, value="", style=wx.TE_MULTILINE|wx.TE_READONLY)
-        hbox2.Add(self.textDisplay, 1, flag=wx.EXPAND)
-        vbox.Add(hbox2, 1, flag=wx.LEFT|wx.RIGHT|wx.EXPAND, border=5)
+        self.textDisplay = wx.TextCtrl(self, value="", style=wx.TE_MULTILINE|wx.TE_READONLY)
+        hbox2.Add(self.textDisplay, proportion=1, flag=wx.EXPAND)
+        self.mainSizer.Add(hbox2, proportion=1, flag=wx.LEFT|wx.RIGHT|wx.EXPAND, border=5)
         
         hbox3 = wx.BoxSizer(wx.HORIZONTAL)
-        btn1 = wx.Button(panel, label="Clear logs")
+        btn1 = wx.Button(self, label="Clear log screen")
         btn1.Bind(wx.EVT_BUTTON, self.OnClear)
-        hbox3.Add(btn1, flag=wx.ALL, border=2)
-        vbox.Add(hbox3, flag=wx.CENTER)
+        hbox3.Add(btn1)
+        self.mainSizer.Add(hbox3, flag=wx.ALIGN_CENTER)
         
         hbox4 = wx.BoxSizer(wx.HORIZONTAL)
-        self.textInput = wx.TextCtrl(panel, value="Test message", style=wx.TE_LEFT|wx.TE_PROCESS_ENTER)
+        self.textInput = wx.TextCtrl(self, value="Test message", style=wx.TE_PROCESS_ENTER)
         self.textInput.Bind(wx.EVT_KEY_DOWN, self.OnEnter)
-        hbox4.Add(self.textInput, 1, flag=wx.ALL, border=2)
-        btn2 = wx.Button(panel, label="Send Message")
+        hbox4.Add(self.textInput, proportion=1)
+        btn2 = wx.Button(self, label="Send Message")
         btn2.Bind(wx.EVT_BUTTON, self.OnSendMsg)
-        hbox4.Add(btn2, flag=wx.ALL, border=2)
-        vbox.Add(hbox4, flag=wx.EXPAND)
+        hbox4.Add(btn2)
+        self.mainSizer.Add(hbox4, flag=wx.ALL|wx.EXPAND, border=5)
         
-        panel.SetSizer(vbox)
-    
-    def OnClose(self, event):
-        """Called when the window close (X) button is pressed"""
-        dlg = wx.MessageDialog(self,
-                               "Are you sure you want to close the application?",
-                               "Confirmation",
-                               wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
-        result = dlg.ShowModal()
-        dlg.Destroy()
-        if result == wx.ID_YES:
-            self.Destroy()
+        hbox5 = wx.BoxSizer(wx.VERTICAL)
+        
+        btn3 = wx.Button(self, label="Log to file")
+        btn3.Bind(wx.EVT_BUTTON, self.OnLog2File)
+        hbox5.Add(btn3, flag=wx.LEFT|wx.RIGHT, border=5)
+        self.pathText = wx.StaticText(self, label="No file selected")
+        hbox5.Add(self.pathText, flag=wx.LEFT|wx.RIGHT, border=5)
+        self.mainSizer.Add(hbox5)
+        
+        self.SetSizer(self.mainSizer)
     
     def OnEnter(self, event):
         """Handle when the return key is pressed within textInput field"""
@@ -191,16 +183,17 @@ class MainWindow(wx.Frame):
     def OnSendMsg(self, event):
         """Called when the send button is pressed"""
         message = self.textInput.Value  # Get string from input field
-        try:
-            print "Sending to {}:{}".format(remote_host, remote_port)
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect(remote_socket)
-            client.send(message)
-            client.shutdown(socket.SHUT_RDWR)
-            client.close()
-        except socket.error as e:
-            print "Socket error: {}".format(e)
-        self.textInput.Clear()  # Clear text input field after trying to send
+        if message: # Only send if non-empty string
+            try:
+                print "Sending to {}:{}".format(remote_host, remote_port)
+                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client.connect(remote_socket)
+                client.send(message)
+                client.shutdown(socket.SHUT_RDWR)
+                client.close()
+                self.textInput.Clear()  # Clear text input field after sending successfully
+            except socket.error as e:
+                print "Socket error: {}".format(e)
     
     def OnClear(self, event):
         """Called when the clear logs button is pressed"""
@@ -209,16 +202,57 @@ class MainWindow(wx.Frame):
     def UpdateDisplay(self, data):
         """Called when the window panel receives an event from pub.sendMessage with data"""
         self.textDisplay.AppendText( str(data) )
+    
+    def OnLog2File(self, event):
+        """Called when the Log to File button is pressed"""
+        dlg = wx.FileDialog(self,
+                            message="Save as...",
+                            defaultDir=os.getcwd(),
+                            wildcard="Log files (*.log)|*.log",
+                            style=wx.FD_SAVE|wx.FD_OVERWRITE_PROMPT)
+        result = dlg.ShowModal()
+        if result == wx.ID_OK:
+            path = dlg.GetPath()
+        dlg.Destroy()
+        self.pathText.SetLabel(str(path))   # Update file path text label
+
+
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title, size=(500, 400)):
+        wx.Frame.__init__(self, parent, title=title, size=size)
+        
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        
+        self.panel = MainPanel(self)
+        self.Center()
+        self.Show()
+        
+        # Start thread to listen for incomming socket connections
+        self.ipc = SocketClient()
+    
+    def OnClose(self, event):
+        """Called when the window close (X) button is pressed"""
+        dlg = wx.MessageDialog(self,
+                               "Are you sure you want to close the application?",
+                               "Confirmation",
+                               wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
+        result = dlg.ShowModal()
+        dlg.Destroy()
+        if result == wx.ID_YES:
+            self.Destroy()
 
 
 if __name__ == "__main__":
     app = wx.App(False)
     
+    local_socket = ('localhost', 8081); remote_socket = ('localhost', 8082)
     frame = MainWindow(None, title="Serial and Socket Logger 1")
+    
+    #local_socket = ('localhost', 8082); remote_socket = ('localhost', 8081)
     #frame2 = MainWindow(None, title="Serial and Socket Logger 2")
     
     # Start serial thread which will monitor serial port
     # and send data to the text display on the GUI window
-    serial = SerialClient(port='COM11', baudrate=9600)
+    #serial = SerialClient(port='COM11', baudrate=9600)
     
     app.MainLoop()
